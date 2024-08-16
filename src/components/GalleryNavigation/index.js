f4341872ba303bd6e8ff500f420cbf3db8d37954
sido <imbilusydney09@gmail.com>
2024-08-16 08:39:31 +0300

    implemented gallery navigation

---
+++ src/components/GalleryNavigation/index.js
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import './GalleryNavigation.css';
+
+function GalleryNavigation({galleries}) {
+    console.log(galleries);
+    return(
+        <nav>
+            <NavLink exact to="/">
+                Home
+            </NavLink>
+            {
+                galleries.map(gallery =>{
+                    return(<NavLink key={gallery.gallerynumber} to={`/galleries/${gallery.id}`}>
+                        {gallery.name}
+                    </NavLink>)
+                })
+            }
+            <h1>Galleries</h1>
+        </nav>
+    )
+}
+
+export default GalleryNavigation;
